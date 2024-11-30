575ec707cdb64e66fc3d8792098258c5a3e9b8b7
owlninjam <owlninjam@gmail.com>
2024-11-30 11:12:12 +0530

    fix: update theme provider types and change dev port to 3334

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,7 +2,18 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+
+type Theme = "dark" | "light" | "system"
+type Attribute = "class" | "data-theme"
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: Attribute
+  defaultTheme?: Theme
+  enableSystem?: boolean
+  storageKey?: string
+  disableTransitionOnChange?: boolean
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
